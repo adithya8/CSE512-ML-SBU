b929ee9de08df913a0be763755353e010b40846f
Adithya V Ganesan <v.adithyaganesan@gmail.com>
2020-09-14 20:47:32 -0400

    HW 1 wrapping up

--- hw1/Solutions/4.py
+++ hw1/Solutions/4.py
@@ -109,13 +109,13 @@ def plot_regression(pred_dict:dict, y:np.ndarray, X:np.ndarray, file_path:str=No
     for i in range(len(pred_dict.keys())):
         p = sorted(list(pred_dict.keys()))[i]
         y_pred = pred_dict[p]
-        axs[i//3, i%3].plot(X, y)
-        axs[i//3, i%3].plot(X, y_pred)
+        axs[i//3, i%3].plot(X[:len(y)], y)
+        axs[i//3, i%3].plot(X[:len(y_pred)], y_pred)
         axs[i//3, i%3].set_title(f"P = {p}", fontsize=15)
         axs[i//3, i%3].grid(axis="x")
         axs[i//3, i%3].tick_params(axis='both', which='major', labelsize=15)
 
-    alpha = "0" if ridge==False else "0.0001*m"
+    alpha = "0" if ridge==False else "1e-7*m"
     fig.suptitle(f"Dew vs Weeks; alpha={alpha}", fontsize=20)
 
     fig.savefig(f"{file_path}", bbox_inches="tight", pad_inches=0.5)
@@ -133,14 +133,14 @@ def plot_regression_alpha(pred_dict:dict, y:np.ndarray, X:np.ndarray, file_path:
     for i in range(len(pred_dict.keys())):
         alpha = sorted(list(pred_dict.keys()))[i]
         y_pred = pred_dict[alpha]
-        axs[i//3, i%3].plot(X, y)
-        axs[i//3, i%3].plot(X, y_pred)
-        alpha_ = np.around(alpha/len(y_pred), decimals=5)
+        axs[i//3, i%3].plot(X[:len(y)], y)
+        axs[i//3, i%3].plot(X[:len(y_pred)], y_pred)
+        alpha_ = np.around(alpha/(len(y)), decimals=8)
         axs[i//3, i%3].set_title(f"alpha = {alpha_}*m", fontsize=15)
         axs[i//3, i%3].grid(axis="x")
         axs[i//3, i%3].tick_params(axis='both', which='major', labelsize=15)
 
-    P = 167
+    P = 6
     fig.suptitle(f"Dew vs Weeks; P={P}", fontsize=20)
 
     fig.savefig(f"{file_path}", bbox_inches="tight", pad_inches=0.5)
@@ -178,7 +178,7 @@ def prediction_over_p(P:list, X:np.ndarray, y:np.ndarray, alpha:float=0):
     for p in P:
         ridge = RidgeRegression(num_dims=p, alpha=alpha)
         X_new = transform_polynomial(p, X)
-        ridge.fit(X_new, y)
+        ridge.fit(X_new[:len(y)], y)
         pred_dict[p] = ridge.predict(X_new)
     
     return pred_dict
@@ -193,7 +193,7 @@ def prediction_over_alpha(P:int, alphas:list, X:np.ndarray, y:np.ndarray):
     X = transform_polynomial(P, X)
     for alpha in alphas:
         ridge = RidgeRegression(num_dims=P, alpha=alpha)
-        ridge.fit(X, y)
+        ridge.fit(X[:len(y)], y)
         pred_dict[alpha] = ridge.predict(X)
     
     return pred_dict
@@ -202,6 +202,7 @@ def prediction_over_alpha(P:int, alphas:list, X:np.ndarray, y:np.ndarray):
 ###########################################
 
 if __name__ == '__main__':
+    ###########################################
     #Load data
     data_dict = load_data()
     print ("Data loaded:")
@@ -210,9 +211,13 @@ if __name__ == '__main__':
         print (i, data_dict[i].shape)
     print ('-----------------------------------------\n')
 
+    ###########################################
     #plot method
     plot(data_dict["X"], data_dict["y"])
-    #print ('-----------------------------------------\n')
+    print ('-----------------------------------------\n')
+
+    ###########################################
+    #condition number
     P = [2, 3, 6, 11]
     condition_numbers = []
     for p in P[:]:
@@ -220,48 +225,65 @@ if __name__ == '__main__':
 
     print (f"Condition Numbers")
     print (np.array(condition_numbers))
+    print ('-----------------------------------------\n')
 
+    ###########################################
+    #Linear regression w/o regularization
     P = [2, 3, 11, 101, 151, 201]
     predictions_linreg = prediction_over_p(P, alpha=0, X=data_dict["X"], y=data_dict["y"])
     plot_regression(predictions_linreg, data_dict["y"], data_dict["X"])
 
+    ###########################################
+    #Linear reg with regularization
     predictions_ridgereg = prediction_over_p(P, alpha=0.0001 * len(data_dict["y"]), X=data_dict["X"], y=data_dict["y"])
     plot_regression(predictions_ridgereg, data_dict["y"], data_dict["X"], ridge=True)
     
-    P = [101, 117, 149, 167, 185, 201]
-    predictions_ridgereg_p = prediction_over_p(P, alpha=0.0001 * len(data_dict["y"]), X=data_dict["X"][:-1], y=data_dict["y"][1:])
-    plot_regression(predictions_ridgereg_p, data_dict["y"][1:], data_dict["X"][:-1], ridge=True, file_path="predictions_ridge_p_search.png")
-
-    alphas = [1* len(data_dict["y"]), 0.1* len(data_dict["y"]), 0.01* len(data_dict["y"]), \
-              0.001* len(data_dict["y"]), 0.0001* len(data_dict["y"]), 0.00001* len(data_dict["y"])] 
-
-    predictions_ridgereg_alpha = prediction_over_alpha(P = 167, alphas=alphas, X=data_dict["X"][:-1], y=data_dict["y"][1:])
-    plot_regression_alpha(predictions_ridgereg_alpha, data_dict["y"][1:], data_dict["X"][:-1], file_path="predictions_ridge_alpha_search.png")
-
+    ###########################################
+    #Linear reg with regularization for performing day ahead predictions. 
     X_new = [data_dict["X"][-1] + (data_dict["X"][1] - data_dict["X"][0]), ]
-    for i in range(1, 100):
+    #Creating future values for X
+    for i in range(1, 280):
         X_new.append(X_new[i-1] + (data_dict["X"][1] - data_dict["X"][0]))
-    
-    
-    P=6
-    ridge = RidgeRegression(num_dims=P, alpha=0.000000001*len(data_dict["y"]))
+    X_train_test = np.array(data_dict["X"].tolist() + X_new).reshape(-1,)
+    print (X_train_test[-10:])
+
+    #First searching through P for an optimal value by setting a stern regularization param(alpha=1e-7)
+    P = [2, 3, 4, 5, 6, 7]
+    predictions_ridgereg_p = prediction_over_p(P, alpha=0.0000001 * len(data_dict["y"]), X=X_train_test[:-1], y=data_dict["y"][1:])
+    plot_regression(predictions_ridgereg_p, data_dict["y"][1:], X_train_test, ridge=True, file_path="predictions_ridge_p_search.png")
+
+    #Searching for the right regularization for the best P(=6) from the above plot.
+    alphas = [0.1* len(data_dict["y"]), 0.01* len(data_dict["y"]), 0.001* len(data_dict["y"]), \
+              0.0001* len(data_dict["y"]), 0.00001* len(data_dict["y"]), 0.0000001 * len(data_dict["y"])] 
+
+    predictions_ridgereg_alpha = prediction_over_alpha(P = 3, alphas=alphas, X=X_train_test[:-1], y=data_dict["y"][1:])
+    plot_regression_alpha(predictions_ridgereg_alpha, data_dict["y"][1:], X_train_test[:-1], file_path="predictions_ridge_alpha_search.png")
+
+    #PLotting the predictions in future for the best P(=6) and alpha(=1e-3, 1e-2). 
+    P=3
     X = transform_polynomial(p=P, X = data_dict["X"])
-    ridge.fit(X, data_dict["y"])
     X_train_test = transform_polynomial(p=P, X = np.array(data_dict["X"].tolist() + X_new).reshape(-1,))
-    pred = ridge.predict(X_train_test)
+    
+    ridge = RidgeRegression(num_dims=P, alpha=0.01*len(data_dict["y"]))
+    ridge.fit(X, data_dict["y"])
+    pred_1 = ridge.predict(X_train_test)
+    ridge = RidgeRegression(num_dims=P, alpha=0.001*len(data_dict["y"]))
+    ridge.fit(X, data_dict["y"])
+    pred_2 = ridge.predict(X_train_test)
 
     fig, ax = plt.subplots(figsize=(15, 15))
-    ax.plot(X[:, 1], data_dict["y"], linewidth=7, label="True")
-    ax.plot(X_train_test[:, 1], pred, linewidth=10, linestyle="dashed", label = "Pred")
+    ax.plot(X[:, 1], data_dict["y"], linewidth=5, label="True", color="gray")
+    ax.plot(X_train_test[:, 1], pred_1, linewidth=10, linestyle="dashed", label = "Pred; alpha=0.01*m")
+    ax.plot(X_train_test[:, 1], pred_2, linewidth=10, linestyle="dotted", label = "Pred; alpha=0.001*m", color="r")
     ax.set_xlabel("Weeks after first reading", fontsize=30)
     ax.set_ylabel(f"Dew Point Temp (C)", fontsize=30)
-    #ax.set_title("", fontsize=40)
+    ax.set_title("P=3", fontsize=40)
     ax.tick_params(axis='both', which='major', labelsize=30)
     ax.grid()
     ax.legend(fontsize=30)
     fig.savefig(f"test.png", bbox_inches="tight", pad_inches=0.5)
     print (f"Plot saved as test.png")
-
+    
 ###########################################
 ###########################################
 ###########################################
